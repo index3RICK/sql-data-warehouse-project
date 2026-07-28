f6c579aa8e4a5915e32361775453d36fd9eb172c
Erick Chicaiza <113222868+index3RICK@users.noreply.github.com>
2026-07-28 06:11:16 -0600

    Refactor date and sales calculations in SQL procedure

--- scripts/Silver/proc_load_silver.sql
+++ scripts/Silver/proc_load_silver.sql
@@ -137,52 +137,29 @@ BEGIN
         sls_ord_num,
         sls_prd_key,
         sls_cust_id,
-
-        CASE
-            WHEN sls_order_dt = 0
-              OR LENGTH(sls_order_dt::TEXT) <> 8
-            THEN NULL
-            ELSE DATE_TRUNC('day',
-                    TO_DATE(sls_order_dt::TEXT,'YYYYMMDD'))::DATE
+        CASE 
+            WHEN sls_order_dt = 0 OR LENGTH(sls_order_dt::TEXT) <> 8 THEN NULL
+            ELSE DATE_TRUNC('day', TO_DATE(sls_order_dt::TEXT,'YYYYMMDD'))::DATE
         END,
-
-        CASE
-            WHEN sls_ship_dt = 0
-              OR LENGTH(sls_ship_dt::TEXT) <> 8
-            THEN NULL
-            ELSE DATE_TRUNC('day',
-                    TO_DATE(sls_ship_dt::TEXT,'YYYYMMDD'))::DATE
+        CASE 
+            WHEN sls_ship_dt = 0 OR LENGTH(sls_ship_dt::TEXT) <> 8 THEN NULL
+            ELSE DATE_TRUNC('day', TO_DATE(sls_ship_dt::TEXT,'YYYYMMDD'))::DATE
         END,
-
         CASE
-            WHEN sls_due_dt = 0
-              OR LENGTH(sls_due_dt::TEXT) <> 8
-            THEN NULL
-            ELSE DATE_TRUNC('day',
-                    TO_DATE(sls_due_dt::TEXT,'YYYYMMDD'))::DATE
+            WHEN sls_due_dt = 0 OR LENGTH(sls_due_dt::TEXT) <> 8 THEN NULL
+            ELSE DATE_TRUNC('day', TO_DATE(sls_due_dt::TEXT,'YYYYMMDD'))::DATE
         END,
-
         CASE
-            WHEN sls_sales IS NULL
-              OR sls_sales <= 0
-              OR sls_sales <> sls_quantity * ABS(sls_price)
-            THEN sls_quantity * ABS(sls_price)
+            WHEN sls_sales IS NULL OR sls_sales <= 0 OR sls_sales <> sls_quantity * ABS(sls_price) THEN sls_quantity * ABS(sls_price)
             ELSE sls_sales
         END,
-
         sls_quantity,
-
         CASE
-            WHEN sls_price IS NULL
-              OR sls_price <= 0
-            THEN COALESCE(
-                    sls_sales / NULLIF(sls_quantity,0),
-                    0
-                 )
+            WHEN sls_price IS NULL OR sls_price <= 0 THEN COALESCE( sls_sales / NULLIF(sls_quantity,0), 0)
             ELSE sls_price
         END
-
-    FROM bronze.crm_sales_details;
+    FROM bronze.crm_sales_details
+    WHERE sls_order_dt > 19000101 AND LENGTH(sls_order_dt::text)=8;
 
     end_time := clock_timestamp();
 
